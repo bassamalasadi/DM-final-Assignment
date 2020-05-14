3df0957bcae0c05ab2a08695c0e518179d993909
bassam <n7520@student.jamk.fi>
2020-05-14 23:00:06 +0300

    updating routes

--- middleware/index.js
+++ middleware/index.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   isLoggedIn: (req, res, next) => {
     if (req.isAuthenticated()) return next();
-    req.session.error = "You need to be logged in to do that!";
+    req.session.error = "You need to be logged in!";
     req.session.redirectTo = req.originalUrl;
     res.redirect("/login");
   },
